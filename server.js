1ccd2c1f3345ba61df4572cf7c0994fb23c73947
ChetanChoudhari25 <cchoudhari205@gmail.com>
2025-09-22 13:36:09 +0530

    added .gitignore

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const port = 3000
+
+app.get('/', async (req, res) => {
+    console.log('Hello World!')
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
